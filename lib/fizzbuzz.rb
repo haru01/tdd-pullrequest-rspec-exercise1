fead6b537eb382ae94395cc519280bd0771ddd1e
eiji.ienaga <eiji.ienaga@gmail.com>
2016-06-09 08:23:15 +0900

    スタート地点を用意した

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 class FizzBuzz
   # TODO 2. リファクタリング 変数名
   # TODO 3. リファクタリング メソッド名 (option)
-  def say(number)
-    return "FizzBuzz" if number % 15 == 0
-    return "Fizz" if number % 3 == 0
-    number.to_s
+  def say(aaa)
+    "2"
   end
+
+  # TODO 不要コメント
 end
